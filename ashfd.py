80f734e59979af471b2124cbc6532909cb6deb76
xphan <xphan@clustertech.com>
2018-07-31 10:35:40 +0800

    test

---
+++ ashfd.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+'''
+# @DATE : 2018/7/31 10:35
+# @Author : xphan
+# @File : ashfd.py
+'''
+#jkjdfk
--- first.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-'''
-# @DATE : 2018/7/31 10:31
-# @Author : xphan
-# @File : first.py
-'''
-def name(self):
-    pass
